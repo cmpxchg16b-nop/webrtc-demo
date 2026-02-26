d0387925a15311bd340f5a0743c72b7a5611cbe0
duststars <i@exploro.one>
2026-02-26 09:08:26 +0800

    add response type definition for openrouter's api

--- webrtc-agents/pkg/handlers/chatbot_dc_handler.go
+++ webrtc-agents/pkg/handlers/chatbot_dc_handler.go
@@ -205,3 +205,64 @@ const exampleOutput string = `
 }
 }
 `
+
+type OpenRouterResponseLine struct {
+	ID                string             `json:"id"`
+	Provider          string             `json:"provider"`
+	Model             string             `json:"model"`
+	Object            string             `json:"object"`
+	Created           int64              `json:"created"`
+	Choices           []OpenRouterChoice `json:"choices"`
+	SystemFingerprint *string            `json:"system_fingerprint"`
+	Usage             *OpenRouterUsage   `json:"usage"`
+}
+
+type OpenRouterChoice struct {
+	Logprobs           interface{}        `json:"logprobs"`
+	FinishReason       string             `json:"finish_reason"`
+	NativeFinishReason string             `json:"native_finish_reason"`
+	Index              int                `json:"index"`
+	Message            *OpenRouterMessage `json:"message"`
+}
+
+type OpenRouterMessage struct {
+	Role             string                      `json:"role"`
+	Content          string                      `json:"content"`
+	Refusal          *string                     `json:"refusal"`
+	Reasoning        string                      `json:"reasoning"`
+	ReasoningDetails []OpenRouterReasoningDetail `json:"reasoning_details"`
+}
+
+type OpenRouterReasoningDetail struct {
+	Format string `json:"format"`
+	Index  int    `json:"index"`
+	Type   string `json:"type"`
+	Text   string `json:"text"`
+}
+
+type OpenRouterUsage struct {
+	PromptTokens            int                                `json:"prompt_tokens"`
+	CompletionTokens        int                                `json:"completion_tokens"`
+	TotalTokens             int                                `json:"total_tokens"`
+	Cost                    float64                            `json:"cost"`
+	IsByok                  bool                               `json:"is_byok"`
+	PromptTokensDetails     *OpenRouterPromptTokensDetails     `json:"prompt_tokens_details"`
+	CostDetails             *OpenRouterCostDetails             `json:"cost_details"`
+	CompletionTokensDetails *OpenRouterCompletionTokensDetails `json:"completion_tokens_details"`
+}
+
+type OpenRouterPromptTokensDetails struct {
+	CachedTokens int `json:"cached_tokens"`
+	AudioTokens  int `json:"audio_tokens"`
+}
+
+type OpenRouterCostDetails struct {
+	UpstreamInferenceCost            float64 `json:"upstream_inference_cost"`
+	UpstreamInferencePromptCost      float64 `json:"upstream_inference_prompt_cost"`
+	UpstreamInferenceCompletionsCost float64 `json:"upstream_inference_completions_cost"`
+}
+
+type OpenRouterCompletionTokensDetails struct {
+	ReasoningTokens int `json:"reasoning_tokens"`
+	AudioTokens     int `json:"audio_tokens"`
+}
