e2a11cdc5f534e15b39180804f4fecdfa73e6f2d
duststars <i@exploro.one>
2026-03-04 17:10:16 +0800

    update

--- webrtc-server/pkg/handler/handle_session.go
+++ webrtc-server/pkg/handler/handle_session.go
@@ -1,19 +1,36 @@
 package handler
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+)
 
 type CtxSessionKey string
 
+type SessionManager interface {
+	// If no associated is found with such request, returns an empty string
+	// otherwise returns a session identifier (which is opaque to the user)
+	GetSessionId(ctx context.Context, r *http.Request) string
+
+	// Associate the request with a session, maybe alter the response when needed.
+	CreateSession(ctx context.Context, w http.ResponseWriter, r *http.Request) string
+}
+
 type SessionHandler struct {
 	origin http.Handler
+	sess   SessionManager
 }
 
 func (h *SessionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// this is a http middleware, it does the following:
+	// 1. check if there is some session associated with request, if there is, set the session identifier to context (key is CtxSessionKey)
+	// 2. otherwise, create a session with request, and set the session identifier of the newly created session to the ctx
 	h.origin.ServeHTTP(w, r)
 }
 
-func WithSessionHandler(origin http.Handler) http.Handler {
+func WithSessionHandler(origin http.Handler, sessionMngr SessionManager) http.Handler {
 	return &SessionHandler{
 		origin: origin,
+		sess:   sessionMngr,
 	}
 }
