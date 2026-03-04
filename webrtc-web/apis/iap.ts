f4fcf219ef4f1e9c0ec9ff4bf5617d3130f17677
duststars <i@exploro.one>
2026-03-04 09:56:27 +0800

    added getAvatar method on IAPOperator

--- webrtc-web/apis/iap.ts
+++ webrtc-web/apis/iap.ts
@@ -4,11 +4,21 @@ export interface IAPOperator {
   getAvatar(username: string): Promise<DataURL>;
 }
 
-export function mockIAPOperator() {
+export function mockIAPOperator(): IAPOperator {
   return {
-    getAvatar(username: string): Promise<DataURL> {
-      // todo: mock it here
-      return Promise.reject("unimplemented yet");
+    async getAvatar(username: string): Promise<DataURL> {
+      const response = await fetch(
+        `https://avatars.githubusercontent.com/${username}`,
+      );
+
+      const blob = await response.blob();
+
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result as DataURL);
+        reader.onerror = reject;
+        reader.readAsDataURL(blob);
+      });
     },
   };
 }
