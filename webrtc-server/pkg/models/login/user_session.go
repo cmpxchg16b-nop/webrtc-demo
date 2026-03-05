4958f6af964d32df15db4cecf8132d0b886bb8c3
duststars <i@exploro.one>
2026-03-05 19:07:24 +0800

    add user store

--- webrtc-server/pkg/models/login/user_session.go
+++ webrtc-server/pkg/models/login/user_session.go
@@ -1,6 +1,9 @@
 package login
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 type UserSessionManager interface {
 	// Login is to associate a session with a registered user
@@ -12,4 +15,29 @@ type UserSessionManager interface {
 	// Returns an empty string if the user hasn't log in, otherwise returns an non-empty string
 	// Callers should check error first
 	GetUserIdBySessionId(ctx context.Context, sessionId string) (string, error)
+}
+
+type MemoryUserSessionManager struct {
+	store sync.Map
+}
+
+// LogIn associates a session with a registered user
+func (m *MemoryUserSessionManager) LogIn(ctx context.Context, userId string, sessionId string) error {
+	m.store.Store(sessionId, userId)
+	return nil
+}
+
+// LogOut de-associates a session with the user bound
+func (m *MemoryUserSessionManager) LogOut(ctx context.Context, sessionId string) error {
+	m.store.Delete(sessionId)
+	return nil
+}
+
+// GetUserIdBySessionId returns the user ID associated with the session
+// Returns an empty string if the user hasn't logged in
+func (m *MemoryUserSessionManager) GetUserIdBySessionId(ctx context.Context, sessionId string) (string, error) {
+	if v, ok := m.store.Load(sessionId); ok {
+		return v.(string), nil
+	}
+	return "", nil
 }
--- webrtc-server/pkg/models/user/user.go
+++ webrtc-server/pkg/models/user/user.go
@@ -1,6 +1,11 @@
 package user
 
-import "context"
+import (
+	"context"
+	"sync/atomic"
+
+	"github.com/google/uuid"
+)
 
 type User struct {
 	Id          string `json:"id"`
@@ -10,9 +15,89 @@ type User struct {
 	GithubId    string `json:"github_id"`
 }
 
+func (u *User) Clone() *User {
+	newUser := new(User)
+	*newUser = *u
+	return newUser
+}
+
 type UserManager interface {
 	// returns (theUser, created, error)
 	LoadOrCreateNewUserByGithubId(ctx context.Context, githubId string, newUser User) (User, bool, error)
 
 	GetUserById(ctx context.Context, userId string) (*User, error)
+}
+
+type InMemoryUserStore struct {
+	Revision int
+	Users    []User
+}
+
+func (store *InMemoryUserStore) Clone() *InMemoryUserStore {
+	newStore := new(InMemoryUserStore)
+	if store != nil {
+		*newStore = *store
+		newStore.Revision += 1
+		newStore.Users = make([]User, len(store.Users))
+		for i := range store.Users {
+			newStore.Users[i] = *store.Users[i].Clone()
+		}
+	}
+	return newStore
+}
+
+func (store *InMemoryUserStore) AddUser(user User) *InMemoryUserStore {
+	newStore := store.Clone()
+	newStore.Users = append(newStore.Users, user)
+	return newStore
+}
+
+type MemoryUserManager struct {
+	store atomic.Pointer[InMemoryUserStore]
+}
+
+func (memUserMngr *MemoryUserManager) doAddUser(user User) {
+	for {
+		oldStore := memUserMngr.store.Load()
+		if memUserMngr.store.CompareAndSwap(oldStore, oldStore.AddUser(user)) {
+			break
+		}
+	}
+}
+
+func (memUserMngr *MemoryUserManager) doCreateUser(user User) User {
+	user.Id = uuid.NewString()
+	memUserMngr.doAddUser(user)
+	return user
+}
+
+func (memUserMngr *MemoryUserManager) LoadOrCreateNewUserByGithubId(ctx context.Context, githubId string, newUser User) (User, bool, error) {
+
+	if user, _ := memUserMngr.GetUserGithubId(ctx, githubId); user != nil {
+		return *user, false, nil
+	}
+
+	return memUserMngr.doCreateUser(newUser), true, nil
+}
+
+func (memUserMngr *MemoryUserManager) GetUserById(ctx context.Context, userId string) (*User, error) {
+	if store := memUserMngr.store.Load(); store != nil {
+		for _, u := range store.Users {
+			if u.Id == userId {
+				return &u, nil
+			}
+		}
+	}
+	return nil, nil
+}
+
+func (memUserMngr *MemoryUserManager) GetUserGithubId(ctx context.Context, githubId string) (*User, error) {
+	if store := memUserMngr.store.Load(); store != nil {
+		for _, u := range store.Users {
+			if u.GithubId == githubId {
+				return &u, nil
+			}
+		}
+	}
+	return nil, nil
 }
